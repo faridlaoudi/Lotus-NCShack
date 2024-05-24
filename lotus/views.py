4184f1cb291965b3fec727cb0b8127cccca56638
faridlaoudi <farid.laoudi@etu.usthb.dz>
2024-05-24 19:09:02 +0100

    login and regestration authentication

---
+++ lotus/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "error": "Invalid Credentials"
+            }, status=status.HTTP_401_UNAUTHORIZED)
